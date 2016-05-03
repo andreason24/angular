8517cb0d9d4c5f9a53b1a0a745d9b617e56ac843
and_reason <andreason24@gmail.com>
2016-05-03 17:37:37 +0300

    update with new endpoint

--- chapter6/js/login/controllers/LoginController.js
+++ chapter6/js/login/controllers/LoginController.js
@@ -1,24 +1,21 @@
-App.controller('LoginController',function($scope, $resource, $http, $location, $filter, $routeParams, $base64, AuthService){
-  var User = $resource('http://angular.codeforges.com/api/wp-json/wp/v2/users/:id');
+App.controller('LoginController',function($scope, $http, $location, $filter, $routeParams, $base64, AuthService){
 
   $scope.user = AuthService.currentUser();
-  $scope.users = User.query();
 
   $scope.login = function (){
-	var guest = $filter('filter')($scope.users, {name: $scope.userName})[0];
 
-	if (guest) {
-		$http({
-	        	method: "POST",
-	        	url: 'http://angular.codeforges.com/api/wp-json/wp/v2/users/'+ guest.id,
-	      		headers: {
-          		  Authorization: 'Basic ' + $base64.encode( $scope.userName + ':' + $scope.password )
-        		}
-	      })
-		  .then(function (response, status){
-	          AuthService.login(response.data)
-	          $location.path( "/" );
-	   	  });
-  	}
-  }
+	$http({
+        	method: "GET",
+        	url: 'http://angular.codeforges.com/api/wp-json/wp/v2/users/me?_envelope',
+      		headers: {
+      		  Authorization: 'Basic ' + $base64.encode( $scope.userName + ':' + $scope.password )
+    		}
+      })
+	  .then(function (response, status){
+	  	if (response.data.status == 302) {
+          AuthService.login(response.data.body)
+          $location.path( "/" );
+	  	}
+   	  });
+}
 });
